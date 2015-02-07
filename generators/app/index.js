f64b5ef0a8db681a6e424284fbc6419959e1138a
Stian Drøbak <sum41rules4@hotmail.com>
2015-02-07 19:27:15 +0100

    added bower and options

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,8 @@ var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
   paths:{
-    app: 'app'
+    app: 'app',
+    bower_components: 'bower_components'
   },
   prompting: function(){
     var done = this.async();
@@ -20,18 +21,63 @@ module.exports = generators.Base.extend({
     }.bind(this));
 
   },
+
+
   prompting2: function(){
     var done = this.async();
+
+    // project name
     this.prompt({
-      type: 'input',
-      name: 'fire',
-      message: 'Should I fire you?',
-      default: 'yes'
+      type: 'checkbox',
+      name: 'choices',
+      message: 'Do you want to use any of these packages?',
+      choices: [
+        {name: 'SASS', checked: this.options.sass},
+        {name: 'CoffeeScript', checked: this.options.coffee},
+        {name: 'Bootstrap', checked: this.options.bootstrap},
+        {name: 'Ember', checked: this.options.ember},
+        {name: 'jQuery', checked: this.options.jQuery}
+      ]
     }, function(answers){
-      this.log(answers.fire);
+      this.log(answers.choices);
+      
+      // Reset selectable options
+      this.options.coffee = false;
+      this.options.sass = false;
+      this.options.bootstrap = false;
+      this.options.ember = false;
+      this.options.jQuery = false;
+
+
+      for(var i = 0; i < answers.choices.length; i++){
+        var choice = answers.choices[i];
+        if(choice === 'CoffeeScript'){this.options.coffee = true;}
+        else if(choice === 'SASS'){this.options.sass = true;}
+        else if(choice === 'Bootstrap'){this.options.bootstrap = true; this.options.jQuery = true;}
+        else if(choice === 'Ember'){this.options.ember = true; this.options.jQuery = true}
+        else if(choice === 'jQuery'){this.options.jQuery = true}
+      }
       done();
     }.bind(this));
+
   },
+
+  prompting3: function(){
+    var done = this.async();
+
+    // project name
+    this.prompt({
+      type: 'input',
+      name: 'folder',
+      message: 'Specify bower component folder name',
+      default: 'bower_components'
+    }, function(answers){
+      this.paths.bower_components = answers.folder;
+      done();
+    }.bind(this));
+
+  },
+
 
 
   constructor: function(){
@@ -59,23 +105,26 @@ module.exports = generators.Base.extend({
       defaults: false
     });
 
-
     this.option('install',{
       desc: 'Install npm and bower dependencies',
       type: 'Boolean',
       defaults: false
     });
 
-  },
-  method1: function(){console.log("method 1 just ran");},
-  method2: function(){console.log("method 2 just ran");},
+    this.option('jQuery',{
+      desc: 'Include jQuery (including bootstrap, or ember will include it automatically)',
+      type: 'Boolean',
+      defaults: false
+    });
 
+  },
 
   packageJSON: function(){
     this.template('_package.json', 'package.json');
   },
   packageBower: function(){
     this.template('_bower.json', 'bower.json');
+    this.template('_bowerrc', '.bowerrc');
   },
   generateIndex: function(){
     this.template('index.php', '<%= paths.app %>/index.php');
--- generators/app/templates/index.php
+++ generators/app/templates/index.php
@@ -2,9 +2,28 @@
 <html>
   <head>
     <title>Welcome to <%= appname %></title>
+    <meta charset="utf-8" />
+    <!-- remove in dist -->
+    <% if (options.bootstrap) { %><link rel="stylesheet" type="text/css" href="<%= paths.bower_components %>/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<%= paths.bower_components %>/bootstrap/dist/css/bootstrap-theme.css">
+    <% }%>
+    
+    <!-- end remove in dist -->
   </head>
   <body>
-    <h1>Welcome to <%= appname %></h1>
-    <p>Welcome to the php scaffold of the app <%= appname %></p>
+    <div<% if(options.bootstrap) { %> class="container-fluid"<% } %>>
+      <h1>Welcome to <%= appname %></h1>
+      <p>Welcome to the php scaffold of the app <%= appname %></p>
+
+      <!-- remove in dist -->
+      <% if (options.jquery || options.bootstrap || options.ember ) { %><script src="<%= paths.bower_components %>/jquery/dist/jquery.js"></script>
+      <% } if (options.bootstrap) { %><script src="<%= paths.bower_components %>/bootstrap/dist/js/bootstrap.js"></script>
+      <% } if (options.ember) {  %><script src="<%= paths.bower_components %>/handlebars/handlebars.js"></script>
+      <script src="<%= paths.bower_components %>/ember/ember.js"></script>
+      <script src="<%= paths.bower_components %>/ember-data/ember-data.js></script>
+      <% } %>
+      
+      <!-- end remove in dist -->
+    </div>
   </body>
 </html>
