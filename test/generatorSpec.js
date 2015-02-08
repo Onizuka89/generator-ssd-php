5b7a29188cb6e41f8d4af9006b3b029cfa50b03f
Stian Drøbak <sum41rules4@hotmail.com>
2015-02-08 18:57:44 +0100

    wrapped test to run different scenarioes

--- test/generatorSpec.js
+++ test/generatorSpec.js
@@ -1,59 +1,143 @@
-describe('Mocha/Grunt generator test suite', function(){
-  var yeoman = require('yeoman-generator');
-  var path = require('path');
-  var fs = require('fs-extra');
+var _ = require('underscore');
+var should = require('should');
+var testData = [
 
-  var assert;
-  var mockGen;
+{
+  title: "Test of ssd-php generator with no parameters",
+  arguments: [
+    'ssd-php'
+  ],
+  prompt:{
+    choices: [
+    ]
+  }
+},
 
-  before(function(done){
-    assert = yeoman.assert;
-    mockGen = yeoman.test;
+{
+  title: "Test of ssd-php generator with SASS",
+  arguments: [
+    'ssd-php'
+  ],
+  prompt:{
+    choices: [
+      'SASS'
+    ]
+  }
+},
 
-    mockGen.run(path.join(__dirname, '../generators/app'))
-      .inDir(path.join(__dirname, './tmp'))
-      .withArguments(['ssd-php'])
-      .withPrompt({
-        choices: ['Ember','SASS','CoffeeScript', 'Bootstrap']
-      })
-      .on('end', done);
-  });
+{
+  title: "Test of ssd-php generator with jQuery",
+  arguments: [
+    'ssd-php'
+  ],
+  prompt:{
+    choices:[
+      'jQuery'
+    ]
+  }
+}
+];
 
-  describe('Directory Creation',function(){
-    it('should generate a root directory', function(){
-      assert.file('app/');
-    });
-  });
+function haveChoice(obj, str){
+  return _.contains(obj.prompt.choices, str);
+}
+
+for(var i = 0; i < testData.length; i++){
+
+  (function(object){
+    describe(object.title, function(){
+      var yeoman = require('yeoman-generator');
+      var path = require('path');
+      var fs = require('fs-extra');
+
+      var assert;
+      var mockGen;
+
+      before(function(done){
+        assert = yeoman.assert;
+        mockGen = yeoman.test;
 
 
-  describe('File Creation', function(){
-    it('should generate "bower.json"', function(){
-      assert.file('bower.json');
-    });
-
-    it('should generate "package.json"', function(){
-      assert.file('package.json');
-    });
-
-    it('should generate ".bowerrc"', function(){
-    });
-
-    it('should generate "Gruntfile.js"', function(){
-      assert.file('Gruntfile.js')
-    });
-  });
-
-/*
-  after(function(done){
-    var filelocation = path.join(__dirname, './tmp');
-    console.log(filelocation.indexOf('test'));
-    if(filelocation.indexOf("test") != -1){
-      fs.remove(filelocation,function(err){
-        if(err) console.log(err);
-        done();
+        mockGen.run(path.join(__dirname, '../generators/app'))
+          .inDir(path.join(__dirname, './tmp'))
+          .withArguments(object.arguments)
+          .withPrompt(object.prompt)
+          .on('end', done);
       });
-    }
-  });
-*/
 
-});
+      describe('Directory Creation',function(){
+        it('should generate a root directory', function(){
+          assert.file('app/');
+        });
+      });
+
+
+      describe('File Creation', function(){
+        it('should generate "bower.json"', function(){
+          assert.file('bower.json');
+        });
+
+        it('should generate "package.json"', function(){
+          assert.file('package.json');
+        });
+
+        it('should generate ".bowerrc"', function(){
+        });
+
+        it('should generate "Gruntfile.js"', function(){
+          assert.file('Gruntfile.js')
+        });
+      });
+
+      describe('Validate "package.json"', function(){
+        var json = null;
+        it('should parse', function(){
+          var file = fs.readFileSync(path.join(__dirname, './tmp', 'package.json'),"utf8");
+          try{
+            json = JSON.parse(file);
+          }catch(e){
+            json = null;
+            should.fail('JSON was not parsed.');
+          }
+
+        });
+
+        if(haveChoice(object, 'SASS')){
+          it('should include SASS', function(){
+            should.exist(json.devDependencies.sass);
+          });
+        }
+
+      });
+
+      describe('Validate "bower.json"', function(){
+        var json = null;
+        it('should parse', function(){
+          var file = fs.readFileSync(path.join(__dirname, './tmp', 'bower.json'),"utf8");
+          try{
+            json = JSON.parse(file);
+          }catch(e){
+            json = null;
+            should.fail('JSON was not parsed.');
+          }
+        });
+
+      });
+
+
+      /*
+         after(function(done){
+         var filelocation = path.join(__dirname, './tmp');
+         console.log(filelocation.indexOf('test'));
+         if(filelocation.indexOf("test") != -1){
+         fs.remove(filelocation,function(err){
+         if(err) console.log(err);
+         done();
+         });
+         }
+         });
+         */
+
+    });
+  })(testData[i]);
+};
